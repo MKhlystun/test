04fadb5cfeec20561699d0118f9ed47aa5213369
Mikhail Khlystun <m_khlystun@gcore.lu>
2019-11-08 07:17:00 +0200

    Add main.go

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "time"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+    time.Sleep(10 * time.Second)
+    fmt.Fprintf(w, "hello\n")
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+
+    for name, headers := range req.Header {
+        for _, h := range headers {
+            fmt.Fprintf(w, "%v: %v\n", name, h)
+        }
+    }
+}
+
+func main() {
+
+    http.HandleFunc("/hello", hello)
+    http.HandleFunc("/headers", headers)
+
+    http.ListenAndServe(":8090", nil)
+}
